2d9b12e1cf75f67123e14fc85a09ea71aa7e564b
Austin Willoughby <arwigm@rit.edu>
2021-11-10 12:42:39 -0500

    Upload starter code

---
+++ src/controllers/files.js
@@ -0,0 +1,16 @@
+const filedb = require('../models/filestore.js');
+
+const uploadPage = (req, res) => {
+  res.render('upload');
+};
+
+const uploadFile = (req, res) => {
+
+};
+
+const retrieveFile = (req, res) => {
+};
+
+module.exports.uploadPage = uploadPage;
+module.exports.uploadFile = uploadFile;
+module.exports.retrieveFile = retrieveFile;
