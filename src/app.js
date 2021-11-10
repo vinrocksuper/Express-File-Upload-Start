c7328f5d27326236f89045f6d3a981ba4252642c
Austin Willoughby <arwigm@rit.edu>
2021-11-10 12:44:28 -0500

    Removed code for demo

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
-// Import libraries.
 const express = require('express');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
-const fileUpload = require('express-fileupload');
 
-// Setup our router.
 const router = require('./router.js');
 
-// Configure our port and our mongodb instance.
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/ImageUpload';
@@ -21,15 +17,9 @@ mongoose.connect(dbURL, (err) => {
   }
 });
 
-// Setup our express app with compression.
 const app = express();
 app.use(compression());
 
-// Add our fileUpload plugin. This will take any data uploaded with the 'multipart/formdata'
-// encoding type, and add them to the req.files object in our requests.
-app.use(fileUpload());
-
-// Continue adding standard plugins that already exist in DomoMaker, etc.
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
@@ -41,10 +31,8 @@ app.set('views', `${__dirname}/../views`);
 app.disable('x-powered-by');
 app.use(cookieParser());
 
-// Pull in our routes.
 router(app);
 
-// Start the server.
 app.listen(port, (err) => {
   if (err) {
     throw err;
