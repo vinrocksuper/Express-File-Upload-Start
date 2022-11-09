3935883814e46782f6b5930b9b2ea6713571af3e
Austin Willoughby <arwigm@rit.edu>
2022-11-09 10:39:48 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/ImageUpload';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/FileUpload';
 mongoose.connect(dbURL, (err) => {
   if (err) {
     console.log('Could not connect to the database');
